17e1c04f8eba6529f2b83e82411c0668bcfb1887
shoyooyo <shoyooyo@gmail.com>
2019-12-29 13:43:36 +0530

    travis and deploy script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t shoyooyo/project7client:latest -t shoyooyo/project7client:$SHA -f ./client/Dockerfile ./client
+docker build -t shoyooyo/project7server:latest -t shoyooyo/project7server:$SHA -f ./server/Dockerfile ./server
+docker build -t shoyooyo/project7worker:latest -t shoyooyo/project7worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push shoyooyo/project7client:latest 
+docker push shoyooyo/project7server:latest 
+docker push shoyooyo/project7worker:latest 
+
+docker push shoyooyo/project7client:$SHA
+docker push shoyooyo/project7server:$SHA
+docker push shoyooyo/project7worker:$SHA
+
+kubectl apply -f kube8s/
+kubectl set image deployments/server-deployment server=shoyooyo/project7server:$SHA
+kubectl set image deployments/client-deployment client=shoyooyo/project7client:$SHA
+kubectl set image deployments/worker-deployment worker=shoyooyo/project7worker:$SHA
