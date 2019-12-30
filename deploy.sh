6a87889e6eabcdd9350bc4ba12e2d10761b02e55
shoyooyo <shoyooyo@gmail.com>
2019-12-30 12:31:18 +0530

    1

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,10 @@
-docker build -t shoyooyo/project7client:latest -f ./client/Dockerfile ./client
-docker build -t shoyooyo/project7client:$SHA -f ./client/Dockerfile ./client
-docker build -t shoyooyo/project7server:latest -f ./server/Dockerfile ./server
-docker build -t shoyooyo/project7server:$SHA -f ./server/Dockerfile ./server
-docker build -t shoyooyo/project7worker:latest -f ./worker/Dockerfile ./worker
-docker build -t shoyooyo/project7worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t shoyooyo/project7client:latest -t shoyooyo/project7client:$SHA -f ./client/Dockerfile ./client
+docker build -t shoyooyo/project7server:latest -t shoyooyo/project7server:$SHA -f ./server/Dockerfile ./server
+docker build -t shoyooyo/project7worker:latest -t shoyooyo/project7worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push shoyooyo/project7client:latest 
-docker push shoyooyo/project7server:latest 
-docker push shoyooyo/project7worker:latest 
+docker push shoyooyo/project7client:latest
+docker push shoyooyo/project7server:latest
+docker push shoyooyo/project7worker:latest
 
 docker push shoyooyo/project7client:$SHA
 docker push shoyooyo/project7server:$SHA
